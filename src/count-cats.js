f1424b0fb9ddf24b48ccd5d786cd9d60ce0c25a9
Vachek-hooves <vachek0602@gmail.com>
2023-04-12 13:33:33 +0300

    countCats completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,24 +1,27 @@
 
 
-/**
- * Given matrix where you have to find cats by ears "^^"
- *
- * @param {Array<Array>} matrix 
- * @return {Number} count of cats found
- *
- * @example
- * countCats([
- *  [0, 1, '^^'],
- *  [0, '^^', 2],
- *  ['^^', 1, 2]
- * ]) => 3`
- *
- */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
-module.exports = {
-  countCats
+const backyard = [ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ];
+
+
+function countCats (){
+    let cat = '^^';
+    let catsArr =[];
+    let number;
+
+    let single= backyard.flat();
+    console.log(single)
+
+    for (let i=0; i<single.length; i++){
+        let catPart=single[i];
+        // console.log(catPart)
+        if(catPart===cat){
+          catsArr.push(catPart);
+
+          
+        }
+    }
+    number=catsArr.length
+    alert(number)
 };
+countCats(backyard);
