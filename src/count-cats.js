5c196247a28affb6a9a5870545782acf3d5e8f0d
Vachek-hooves <vachek0602@gmail.com>
2023-04-13 11:03:37 +0300

    countCats completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -21,3 +21,25 @@
 //     countCats
 //   };
 const matrix = [ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ];
+
+function countCats (arr){
+  let cat = '^^';
+  let catsArr =[];
+  let Number;
+
+  let single= arr.flat();
+  console.log(single)
+
+  for (let i=0; i<single.length; i++){
+      let catPart=single[i];
+      // console.log(catPart)
+      if(catPart===cat){
+        catsArr.push(catPart);
+
+        
+      }
+  }
+  Number=catsArr.length
+  alert(Number)
+};
+countCats(matrix);
