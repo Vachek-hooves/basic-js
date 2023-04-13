0c5d4bfc44f49339fcb244e33724065fb2f41731
Vachek-hooves <vachek0602@gmail.com>
2023-04-13 11:02:03 +0300

    countCats completed

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,22 +1,22 @@
-
-module.exports = function countCats(arr) {
-    let cat = '^^';
-    let catsArr =[];
-    let Number;
-
-    let single= arr.flat();
-    console.log(single)
-
-    for (let i=0; i<single.length; i++){
-        let catPart=single[i];
-        if(catPart===cat){
-          catsArr.push(catPart); 
-        }
-    }
-    Number=catsArr.length
-    alert(Number)
-};
-
-countCats(matrix);
-
-const matrix = [ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]; 
+// /**
+//  * Given matrix where you have to find cats by ears "^^"
+//  *
+//  * @param {Array<Array>} matrix 
+//  * @return {Number} count of cats found
+//  *
+//  * @example
+//  * countCats([
+//  *  [0, 1, '^^'],
+//  *  [0, '^^', 2],
+//  *  ['^^', 1, 2]
+//  * ]) => 3`
+//  *
+//  */
+//  function countCats(/* matrix */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+  
+//   module.exports = {
+//     countCats
+//   };
