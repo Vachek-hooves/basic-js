7f08faf9047f20804ba3f785d44cc84e74e4cced
Vachek-hooves <vachek0602@gmail.com>
2023-04-18 01:21:31 +0300

    hanoi-tower

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -12,7 +12,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function deleteDigit() {
-  
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
 
 module.exports = {
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -19,6 +19,16 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getMatrixElementsSum(/* matrix */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  // let sum=0;
+
+  //   for (let i=0; i<matrix.length; i++){
+  //     for( let t=0; t<matrix[i].length; t++){
+  //       console.log(matrix[i][t])
+  //         sum+=matrix[i][t]
+  //       }
+  //     }
+  //   return sum;
 }
 
 module.exports = {
